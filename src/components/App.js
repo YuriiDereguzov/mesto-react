193b7a85a53612b5fc25a7e7809327891fe57605
Yurii Dereguzov <DereguzovYE@yandex.ru>
2023-02-23 15:24:49 +0500

    fix: поправить верстку

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import Footer from "./Footer";
 import Header from "./Header";
 import ImagePopup from "./ImagePopup";
 import Main from "./Main";
-import { api } from "../utils.js/Api";
+import { api } from "../utils/Api";
 import { CurrentUserContext } from "../contexts/CurrentUserContext";
 import EditProfilePopup from "./EditProfilePopup";
 import EditAvatarPopup from "./EditAvatarPopup";
